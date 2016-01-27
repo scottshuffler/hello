c4e911496f6e646390c80285a4a7700810ee9309
scott <scottshuffler@gmail.com>
2016-01-27 13:27:06 -0500

    Hello any name

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -1,7 +1,11 @@
 package hello;
 
+/**
+ * Created by Scott Shuffler on 1/25/16.
+ */
+
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("hello, Scott!");
+		System.out.println("Hello, " + args[0] + "!");
 	}
 }
