a2b30cc2210bd0a708dea4eca27c2ba16b8d5ced
scott <scottshuffler@gmail.com>
2016-01-29 17:58:39 -0500

    Starting unit tests

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -1,11 +1,31 @@
 package hello;
 
 /**
- * Created by Scott Shuffler on 1/25/16.
+ * Created by Scott Shuffler.
  */
 
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, " + args[0] + "!");
+		String input = args[0];
+		if (!input.equals("")) {
+			checkCase(input);
+			input = buildHello(input);
+			printHello(input);
+		}
+	}
+
+	public static String buildHello(String input) {
+		return "Hello, " + input + "!";
+	}
+
+	public static void printHello(String input) {
+
+			checkCase(input);
+			System.out.print(input);
+
+	}
+
+	public static String checkCase(String input) {
+		return input;
 	}
 }
--- hello/src/hello/helloTest.java
+++ hello/src/hello/helloTest.java
@@ -1,25 +1,79 @@
 package hello;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by Scott Shuffler on 1/25/16.
+ * Created by Scott Shuffler.
  */
 
 public class helloTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @org.junit.Test
     public void testMain() throws Exception {
+
+    }
+
+
+    @Test
+    public void testCheckCase() throws Exception {
+
+    }
+    @Test
+    public void testBuildHello() throws Exception {
         try {
-            String[] name = new String[10];
-            name[0] = "Scott";
-            hello.main(name);
+            String result = hello.buildHello("Scott");
+            assertEquals("Hello, Scott!", result);
         }
         catch (Exception e) {
-            fail("Uh oh: " + e);
+            fail("Error: " + e);
         }
         finally {
             System.out.println("Test complete");
         }
     }
+    @Test
+    public void testPrintHello() throws Exception {
+        try {
+            String[] name = new String[1];
+            name[0] = "Scott";
+            hello.main(name);
+            assertEquals("Hello, Scott!", outContent.toString());
+
+            outContent.reset();
+
+            String[] nameTwo = new String[1];
+            nameTwo[0] = "";
+            hello.main(nameTwo);
+            assertEquals("", outContent.toString());
+        }
+        catch (Exception e) {
+            fail("Error: " + e);
+        }
+        finally {
+            System.out.println("Test complete");
+        }
+    }
+
+
 }
