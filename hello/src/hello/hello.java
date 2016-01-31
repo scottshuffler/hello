d751e4094d416905369e450079110a7235b1e7b8
scott <scottshuffler@gmail.com>
2016-01-31 18:40:20 -0500

    Added basic comments to hello class

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -9,12 +9,16 @@ public class hello {
 
     public static void main(String[] args) {
         String userName;
+
+        // If no arguments entered it will display the default name.
         if (args.length == 0) {
-            userName = "Scott";
-            userName = checkCase(userName);
-            userName = buildHello(userName);
+            String defaultName = "Scott";
+            userName = buildHello(defaultName);
             printHello(userName);
         }
+
+        // If you use the -me command it will use your specified
+        // computer name and personal name
         else if (args[0].equals("-me")) {
             computerName = args[1];
             userName = args[2];
@@ -23,6 +27,9 @@ public class hello {
             userName = buildHello(userName);
             printHello(userName);
         }
+
+        // If just your name is on the command line it will use it
+        // and the default computer name
         else {
             userName = args[0];
             if (!userName.equals("")) {
@@ -33,15 +40,19 @@ public class hello {
         }
 	}
 
+    // Takes in the name, makes it all lower case
+    // and then makes the first character capitalized
     public static String checkCase(String input) {
         String newInput = input.toLowerCase();
         return newInput.substring(0, 1).toUpperCase() + newInput.substring(1);
     }
 
+    // Builds the print string that includes the computer name
 	public static String buildHello(String input) {
 		return "Hello, " + input + ". My name is " + computerName + ". How are you " + input + "?";
 	}
 
+    // Prints the string built in buildHello
 	public static void printHello(String input) {
 			System.out.print(input);
 	}
